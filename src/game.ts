c43e6d5a9a84118b164905f402014e1117058ed2
Kevin Upton <k_upton@live.com>
2019-05-16 18:26:44 +1000

    updated the state

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,4 @@
-import { GameState, SocketParty, GameEventManager, GameEvent, GameStateModification, ModificationMap } from '@kevupton/game-engine';
-import { not } from 'rxjs/internal-compatibility';
+import { GameEvent, GameEventManager, GameState, ModificationMap, SocketParty } from '@kevupton/game-engine';
 
 export interface GameData {
   count : number;
@@ -9,26 +8,26 @@ export const gameState = new GameState<GameData>({
   count: 1,
 });
 
-
 export const party = new SocketParty<any>({ rtcConfig: {} });
 
-party.message$.subscribe(message => {
-  console.log(message);
-});
+// party.message$.subscribe(message => {
+//   console.log(message);
+// });
 
 export const events = new GameEventManager(gameState, party);
-
+let prevTime : number = Date.now();
 gameState.state$.subscribe(state => {
-  console.log('new state: ', state);
+  console.log('new state: ', state, Date.now() - prevTime);
 });
 
 const Multiply : GameEvent<GameData> = {
   name: 'multiply',
-  calculateModifications (state) : ModificationMap<GameData>  {
+  calculateModifications (state) : ModificationMap<GameData> {
     console.log('cur state: ', state);
-    const tooHigh : ModificationMap<GameData> = { count: ['=', 1] };
-    const notHighEnough : ModificationMap<GameData> = { count: [['+', '0.1'], ['*', 2]] };
-    const modification = state.count > 1000000 ?  tooHigh : notHighEnough;
+    prevTime = Date.now();
+    const tooHigh : ModificationMap<GameData>       = { count: ['=', 1] };
+    const notHighEnough : ModificationMap<GameData> = { count: [['+', 0.1], ['*', 2]] };
+    const modification                              = state.count > 1000000 ? tooHigh : notHighEnough;
     console.log('modification', modification);
     return modification;
   },
